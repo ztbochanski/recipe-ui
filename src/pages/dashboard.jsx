10af3d69c49509dd885515a4673994fbf7208a39
Zach Bochanski <ztbochanski@gmail.com>
2022-09-26 19:16:06 -0700

    add recipe card edit readme

--- src/pages/dashboard.jsx
+++ src/pages/dashboard.jsx
@@ -6,13 +6,23 @@ const Dashboard = () => (
   <Layout>
     <section className='section'>
       <div className='columns is-multiline'>
-        <div className='column is-quarter'>recipe card</div>
-        <div className='column is-quarter'>recipe card</div>
-        <div className='column is-quarter'>recipe card</div>
-        <div className='column is-quarter'>recipe card</div>
-        <div className='column is-quarter'>recipe card</div>
-        <div className='column is-quarter'>recipe card</div>
-        <div className='column is-quarter'>recipe card</div>
+
+        <div className='column is-one-quarter'>
+          <div className="card">
+            <div className="card-image">
+              <figure className="image">
+                dynamic image here
+              </figure>
+            </div>
+            <div className="card-content">
+              <div className="content">
+                <h1 className="title is-4">card content title</h1>
+                <h2 className="subtitle is-6">card content subtitle</h2>
+              </div>
+            </div>
+          </div>
+        </div>
+
       </div>
     </section>
   </Layout>
